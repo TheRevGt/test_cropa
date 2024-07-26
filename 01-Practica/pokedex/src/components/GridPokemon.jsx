92befeddea74a42cbe35d8328220bfa8b6539221
TheRevGt <jgatzul@gmail.com>
2024-07-26 15:04:30 -0600

    adding GridPokemon component
    Adding resource and custom hook useFetch

---
+++ 01-Practica/pokedex/src/components/GridPokemon.jsx
@@ -0,0 +1,23 @@
+import { useFetch } from "../Hook/useFetch"
+import { CardPokemon } from "./CardPokemon"
+
+export const GridPokemon = () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/'
+    const {data, isLoading, errors} = useFetch(url)
+    return (
+    <>
+      <h1 className=" text-3xl font-bold underline">Pokedex</h1>
+      {
+        isLoading
+        ? <p>Cargando</p>
+        : (
+          <div className=" w-full h-full flex flex-wrap space-3 justify-center">
+              {data.results.map((pokemon, index) => (
+                  <CardPokemon key={index} name={pokemon.name} number={index+1} />
+              ))}
+          </div>
+          )
+      }
+    </>
+    )
+}
