ea0e9ec79ae2c2155a065a7efb87e10450e3b64d
TheRevGt <jgatzul@gmail.com>
2024-07-26 15:17:09 -0600

    adding component GridImages
    adding resource to show images

--- 01-Practica/pokedex/src/Routes/PerfilScreen.jsx
+++ 01-Practica/pokedex/src/Routes/PerfilScreen.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom"
 import { useFetch } from "../hooks/useFetch";
+import { GridImages } from "../components/GridImages";
 
 export const PerfilScreen = () => {
     const {name} = useParams();
---
+++ 01-Practica/pokedex/src/components/GridImages.jsx
@@ -0,0 +1,16 @@
+export const GridImages = ({name, sprites = [] }) => {
+    return (
+      <div className=" flex flex-wrap space-3 justify-center border-b-2">
+        {sprites.map( (sprite, index) =>(
+          <div className=" flex w-96 flex-col rounded-xl" key={index}>
+              <img src={sprite}
+                alt={name}
+                className=" w-full "
+              />
+          </div>
+      ))
+    }
+    </div>
+    )
+  }
+  
