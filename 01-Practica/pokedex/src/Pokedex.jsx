076b23e8473eee92aced264248476c50fa36582a
TheRevGt <jgatzul@gmail.com>
2024-07-26 14:53:33 -0600

    adding route in project

--- 01-Practica/pokedex/src/Pokedex.jsx
+++ 01-Practica/pokedex/src/Pokedex.jsx
@@ -6,23 +6,9 @@ export const Pokedex = () => {
     const url = ' https://pokeapi.co/api/v2/pokemon/'
     const {data, isLoading, hasError, errror} = useFetch(url);
   return (
-    <>
-        <h1>Información de Pokémon</h1>
-        <hr />
-        {
-          isLoading
-          ? <p>Cargando</p>
-          : (
-            <ul>
-            {data.results.map((pokemon, index) => (
-            <li key={index}>
-                <a>{pokemon.name}</a>
-
-            </li>
-            ))}
-          </ul>
-            )
-        }
-    </>
+    <Routes>
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/pokemon/:name" element={<PerfilScreen />} />
+    </Routes>
   )
 }
