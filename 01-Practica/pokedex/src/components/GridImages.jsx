164c2001b986cd571d48315aacaf5d7c4b4e9377
TheRevGt <jgatzul@gmail.com>
2024-07-26 20:05:51 -0600

    fixing class from div in map sprite

--- 01-Practica/pokedex/src/components/GridImages.jsx
+++ 01-Practica/pokedex/src/components/GridImages.jsx
@@ -2,7 +2,7 @@ export const GridImages = ({name, sprites = [] }) => {
     return (
       <div className=" flex flex-wrap space-3 justify-center border-b-2">
         {sprites.map( (sprite, index) =>(
-          <div className=" flex w-96 flex-col rounded-xl" key={index}>
+          <div className=" flex w-60 flex-col rounded-xl" key={index}>
               <img src={sprite}
                 alt={name}
                 className=" w-full "
