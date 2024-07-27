35149a5d01aeeaa5c884dd796de4d62403063e0f
TheRevGt <jgatzul@gmail.com>
2024-07-27 11:47:46 -0600

    adding title in component

--- 01-Practica/pokedex/src/Routes/HomeScreen.jsx
+++ 01-Practica/pokedex/src/Routes/HomeScreen.jsx
@@ -2,6 +2,9 @@ import { GridPokemon } from "../components/GridPokemon"
 
 export const HomeScreen = () => {
   return (
-    <GridPokemon/>
+    <section className=" text-center w- full">
+      <h1 className=" font-bold text-4xl md:text-5xl w-full text-gray-900 leading-tight ">Pokedex</h1>
+      <GridPokemon/>
+    </section>
   )
 }
